9e228fec3a05d7a585943d1bc8016c6b6bfb0115
dongtianchibingguner <lan@lanoudeMac-mini-19.local>
2016-12-13 16:05:03 +0800

    泰国freezer芙丽滋

---
+++ js/bot.js
@@ -0,0 +1,29 @@
+var di = document.getElementsByTagName("footer")[0];
+var img = document.getElementById("di").getElementsByTagName("img");
+var weixin = document.getElementById("weixin");
+img[0].onmouseenter = function(){
+	img[0].src = "img/facebook1.png";
+}
+img[1].onmouseenter = function(){
+	img[1].src = "img/xinlang1.png";
+}
+img[2].onmouseenter = function(){
+	img[2].src = "img/weixin1.png";
+}
+img[0].onmouseleave = function(){
+	img[0].src = "img/facebook.png";
+}
+img[1].onmouseleave = function(){
+	img[1].src = "img/xinlang.png";
+}
+img[2].onmouseleave = function(){
+	img[2].src = "img/weixin.png";
+}
+var heights = document.documentElement.clientHeight;
+//$("#erweima").css({
+//	position: "absolute",
+//	bottom: "0px",
+//	width: "100%",
+//	height: heights,
+//	backgroundColor: "rgba(0,0,0,0.7)"
+//})
